f533eb3ffc4b6e2b51d3cbf886eee57a49f3b8f9
Enmanuel Madera <emadera@emanalyticsllc.com>
2022-12-26 00:11:15 -0500

    initial

---
+++ test/pilot.py
@@ -0,0 +1,17 @@
+import pandas as pd 
+import numpy as np 
+
+df = pd.read_csv('data\\us_baby_names.csv')
+
+df1 = df.sort_values('Count', ascending=False)
+
+df2=df1[(df1['Year'] == 2014) & (df1['Gender']=='F')]
+
+#print(df1.groupby('Year').count())
+
+for c in df2.columns: 
+    if c == 'Name': 
+        print(c)
+        
+        
+
